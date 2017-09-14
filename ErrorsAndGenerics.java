0c926589247cf9e925a982144003987f67352b06
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-09-13 20:16:30 -0700

    added first newUser

--- ErrorsAndGenerics.java
+++ ErrorsAndGenerics.java
@@ -1,5 +1,17 @@
 public class ErrorsAndGenerics {
   public static void main(String[] args) {
-    
+    User newUser = new User("bhold@me.com", "bholderman", "Brandon", 27);
+
+    try {
+      newUser.login();
+    } catch(UserException exception) {
+      System.out.println(exception + " " + exception.description);
+    }
+
+    try {
+      newUser.logout();
+    } catch(UserException exception) {
+      System.out.println(exception + " " + exception.description);
+    }
   }
 }
